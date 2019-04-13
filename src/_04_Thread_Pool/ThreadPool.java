cc7a3d18f173ba9c9540daf23e403499ae2f411b
theonak4 <38365756+theonak4@users.noreply.github.com>
2019-04-13 13:42:49 -0700

    Update ThreadPool.java

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,40 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class ThreadPool {
+
+	Thread[] threads;
+	ConcurrentLinkedQueue<Task> taskQueue;
+
+	public ThreadPool(int totalThreads) {
+		taskQueue = new ConcurrentLinkedQueue<Task>();
+		threads = new Thread[totalThreads];
+		for (int i = 0; i < threads.length; i++) {
+			threads[i] = new Thread(new Worker(taskQueue));
+
+		}
+	}
+
+	public void start() {
+		for (int i = 0; i < threads.length; i++) {
+			threads[i].start();
+
+		}
+		for (int i = 0; i < threads.length; i++) {
+			try {
+				threads[i].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public void addTask(Task task) {
+		taskQueue.add(task);
+
+	}
 
 }
